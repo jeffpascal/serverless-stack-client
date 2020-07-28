ce8f6de0db22ca9c8391e9293cf3f154228fee42
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-07-28 13:35:40 +0300

    added identity connection to aws

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
-
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
   return (
     <div className="App container">
-      <Navbar fluid collapseOnSelect bg="light" expand="lg">
-        <Navbar.Brand href="/">Scratch</Navbar.Brand>
+      <Navbar fluid="true" collapseOnSelect bg="light" expand="lg">
+        {/* <Navbar.Header> */}
+        <Navbar.Brand>
+          <Link to="/">Scratch</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* </Navbar.Header> */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+            </Nav.Link>
+
+            <Nav.Link>
+              <LinkContainer to="/login">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
-      <Navbar.Collapse>
-        <Nav pullRight>
-          <NavItem href="/signup">Signup</NavItem>
-          <NavItem href="/login">Login</NavItem>
-        </Nav>
-      </Navbar.Collapse>
       <Routes />
     </div>
   );
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "unic-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://kgazkvyv5h.execute-api.eu-central-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_qLIMKoQp8",
+    APP_CLIENT_ID: "79es7gqs03r271c4rph688emu9",
+    IDENTITY_POOL_ID: "eu-central-1:02be0678-bca9-4424-8d48-ad6d01c4553b",
+  },
+};
