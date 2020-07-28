16c9f25bc60020513afd9895a6df31f36ae4a7eb
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-07-28 12:58:28 +0300

    created client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect bg="light" expand="lg">
+        <Navbar.Brand href="/">Scratch</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      </Navbar>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem href="/signup">Signup</NavItem>
+          <NavItem href="/login">Login</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+      <Routes />
     </div>
   );
 }
