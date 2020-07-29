a038c35e4d20d7256f3fa052247017d7bec1635d
Pascal Jean-Francois <jeffpascal96@gmail.com>
2020-07-29 21:01:39 +0300

    fixed route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
-import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
+
+import { AppContext } from "./libs/contextLib";
+import "./App.css";
+
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
   return (
     <div className="App container">
       <Navbar fluid="true" collapseOnSelect bg="light" expand="lg">
@@ -15,28 +24,37 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         {/* </Navbar.Header> */}
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link>
-              <LinkContainer to="/signup">
-                <NavItem>Signup</NavItem>
-              </LinkContainer>
-            </Nav.Link>
-
-            <Nav.Link>
-              <LinkContainer to="/login">
-                <NavItem>Signup</NavItem>
-              </LinkContainer>
-            </Nav.Link>
-          </Nav>
           <Nav>
+            {isAuthenticated ? (
+              <Nav.Link onClick={handleLogout}>
+                <NavItem>Log out</NavItem>
+              </Nav.Link>
+            ) : (
+              <>
+                <Nav.Link>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                </Nav.Link>
+                <Nav.Link>
+                  <LinkContainer to="/login">
+                    <NavItem>Signin</NavItem>
+                  </LinkContainer>
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          {/* <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
               Dank memes
             </Nav.Link>
-          </Nav>
+          </Nav> */}
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/containers/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import { useAppContext } from "../libs/contextLib";
+import "./Login.css";
+
+export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </Form.Group>
+        <Button block disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
